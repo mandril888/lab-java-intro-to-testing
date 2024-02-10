9ec748e888ae5d660cde28d3c4b2c2bf46d8797c
Arturo Rubio <arubio@seocom.agency>
2024-02-10 10:38:37 +0100

    WLC

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,25 +1,18 @@
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
-    private Main main;
-
-    @BeforeEach
-    public void setUp() {
-        main = new Main();
-    }
 
     @Test
     @DisplayName("test pass_num_and_generate_arrOdds")
     public void pass_num_and_generate_arrOdds() {
-        assertArrayEquals(new int[]{1}, main.arrOdds(1));
-        assertArrayEquals(new int[]{1}, main.arrOdds(2));
-        assertArrayEquals(new int[]{1, 3, 5}, main.arrOdds(5));
-        assertArrayEquals(new int[]{1, 3, 5}, main.arrOdds(6));
-        assertArrayEquals(new int[]{1, 3, 5, 7, 9}, main.arrOdds(9));
-        assertArrayEquals(new int[]{1, 3, 5, 7, 9}, main.arrOdds(10));
+        assertArrayEquals(new int[]{1}, Main.arrOdds(1));
+        assertArrayEquals(new int[]{1}, Main.arrOdds(2));
+        assertArrayEquals(new int[]{1, 3, 5}, Main.arrOdds(5));
+        assertArrayEquals(new int[]{1, 3, 5}, Main.arrOdds(6));
+        assertArrayEquals(new int[]{1, 3, 5, 7, 9}, Main.arrOdds(9));
+        assertArrayEquals(new int[]{1, 3, 5, 7, 9}, Main.arrOdds(10));
     }
 }
