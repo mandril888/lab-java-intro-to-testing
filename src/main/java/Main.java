6dc3cfb2ed19a8d23ae75d9468b30efb481a22d2
Arturo Rubio <arubio@seocom.agency>
2024-02-10 12:49:48 +0100

    refactor isJavaKeyword() method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println(isJavaKeyword("I love to breakdance"));
     }
 
+    // method that takes in an integer n and returns an array of all odd integers from 1 to n
     public static int[] arrOdds(int num) {
         if(num%2 != 0) num += 1;
         int[] arr = new int[(num)/2];
@@ -17,20 +18,19 @@ public class Main {
         return arr;
     }
 
+    // method that takes in a String and returns true if the String contains any Java Keywords
     public static boolean isJavaKeyword(String sentence) {
-        boolean isJavaKeyword = false;
         String[] listOfKeywords = {"abstract","continue","for","new","switch","assert","default","goto","package","synchronized","boolean","do","if","private","this","break","double","implements","protected","throw","byte","else","import","public","throws","case","enum","instanceof","return","transient","catch","extends","int","short","try","char","final","interface","static","void","class","finally","long","strictfp","volatile","const","float","native","super","while"};
         String[] sentenceArr = sentence.split(" ");
 
         for(String word : sentenceArr) {
             for(String keyword : listOfKeywords) {
                 if(keyword.equals(word)) {
-                    isJavaKeyword = true;
-                    break;
+                    return true;
                 }
             }
         }
 
-        return isJavaKeyword;
+        return false;
     }
 }
